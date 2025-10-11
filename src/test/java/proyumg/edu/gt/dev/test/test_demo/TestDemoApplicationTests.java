c2318efb762cd1d1db27288884c002340a9f9467
cesars <nada@gmail.com>
2025-10-10 23:55:13 -0600

    subiendo proyecto Spring Boot

---
+++ src/test/java/proyumg/edu/gt/dev/test/test_demo/TestDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package proyumg.edu.gt.dev.test.test_demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
